8d6de5ce25016e8a30ceda29253248719b782376
Chao Zhang <chao.zh@gmail.com>
2020-11-21 17:49:15 +0000

    update pub url

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/ub-pub/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/ub-pub/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/~chao/uboone/publications/' : '/',
   "transpileDependencies": [
     "vuetify"
   ]
