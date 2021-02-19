794c97c0b780e7d15ad4915fb992d1b63a522a6d
Chao Zhang <chao.zh@gmail.com>
2021-02-19 18:58:30 +0000

    move production site

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // publicPath: process.env.NODE_ENV === 'production' ? '/ub-pub/' : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/~chao/uboone/publications/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/~chao/uboone/publications/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/at_work/publications/' : '/',
   "transpileDependencies": [
     "vuetify"
   ]
